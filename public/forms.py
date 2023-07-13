38c3543b7c375e698e5d04d0597724a099635dda
ArunPS1999 <118235219+ArunPS1999@users.noreply.github.com>
2023-07-13 20:44:46 +0530

    Update forms.py

--- public/forms.py
+++ public/forms.py
@@ -1,8 +1,1 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
-
-class UserAddForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ["full_name","phone_number","email","country","password1","password2"]
